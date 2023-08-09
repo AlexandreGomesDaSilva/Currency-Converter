ce5c7fa1987169960209eac5cfb860332b828d8e
AlexandreGomesDaSilva <alexgds92@gmail.com>
2023-08-09 16:31:09 +0200

    Move button as a component + it triggers the treatment defined in app + adding style to the button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,19 +2,36 @@ import React from 'react';
 import Header from '../Header/Header';
 import Currencies from '../Currencies/Currencies';
 import Amount from '../Amount/Amount';
+import Button from '../Button/Button';
 import './App.scss';
 
 import currencies from '../../data/currencies';
 
 class App extends React.Component {
+  // Constructor to initialize the component state
   constructor(props) {
     super(props);
 
+    // Initializing the component state
     this.state = {
-      currenciesOpen: false,
+      currenciesOpen: true,
     };
+    // Binding the 'handleClick' method to the current instance of the class
+    // This is necessary to preserve the correct context of 'this' inside the method
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  handleClick() {
+    // Retrieve the current value of currenciesOpen from the state
+    const { currenciesOpen } = this.state;
+    console.log('click !');
+    this.setState({
+      // Toggle the value of currenciesOpen and update the state
+      currenciesOpen: !currenciesOpen,
+    });
+  }
+
+  // Render method to render the component's UI
   render() {
     const { currenciesOpen } = this.state;
 
@@ -22,18 +39,7 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <main>
-          <button
-            type="button"
-            onClick={() => {
-              console.log('Button pressed');
-
-              this.setState({
-                currenciesOpen: !currenciesOpen,
-              });
-            }}
-          >
-            Display/Hide currencies
-          </button>
+          <Button clickTreatment={this.handleClick} isOpen={currenciesOpen} />
           {currenciesOpen && <Currencies currenciesList={currencies} />}
           <Amount />
         </main>
