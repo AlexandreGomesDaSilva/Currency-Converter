160f844a479cd2ac78b0d29f6b82202d38241401
AlexandreGomesDaSilva <alexgds92@gmail.com>
2023-08-09 16:19:56 +0200

    Add state and button functionality to toggle currencies display

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from '../Header/Header';
 import Currencies from '../Currencies/Currencies';
 import Amount from '../Amount/Amount';
@@ -5,14 +6,40 @@ import './App.scss';
 
 import currencies from '../../data/currencies';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Currencies currenciesList={currencies} />
-      <Amount />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currenciesOpen: false,
+    };
+  }
+
+  render() {
+    const { currenciesOpen } = this.state;
+
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <button
+            type="button"
+            onClick={() => {
+              console.log('Button pressed');
+
+              this.setState({
+                currenciesOpen: !currenciesOpen,
+              });
+            }}
+          >
+            Display/Hide currencies
+          </button>
+          {currenciesOpen && <Currencies currenciesList={currencies} />}
+          <Amount />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
